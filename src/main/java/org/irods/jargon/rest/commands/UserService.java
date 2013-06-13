17606f49bcfa130d50108aeedb541dbb1205bb81
mconway <michael_conway@unc.edu>
2013-06-13 13:37:58 -0400

    [#1478] initial dev and setup

--- src/main/java/org/irods/jargon/rest/commands/UserService.java
+++ src/main/java/org/irods/jargon/rest/commands/UserService.java
@@ -3,19 +3,27 @@
  */
 package org.irods.jargon.rest.commands;
 
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 
+import org.irods.jargon.core.exception.JargonException;
 import org.irods.jargon.core.pub.IRODSAccessObjectFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Restful services for iRODS users
  * 
  * @author Mike Conway - DICE (www.irods.org)
- *
+ * 
  */
 @Path("/user")
 public class UserService {
-	
+
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
 	IRODSAccessObjectFactory irodsAccessObjectFactory;
 
 	/**
@@ -26,11 +34,18 @@ public class UserService {
 	}
 
 	/**
-	 * @param irodsAccessObjectFactory the irodsAccessObjectFactory to set
+	 * @param irodsAccessObjectFactory
+	 *            the irodsAccessObjectFactory to set
 	 */
 	public void setIrodsAccessObjectFactory(
-			IRODSAccessObjectFactory irodsAccessObjectFactory) {
+			final IRODSAccessObjectFactory irodsAccessObjectFactory) {
 		this.irodsAccessObjectFactory = irodsAccessObjectFactory;
+	}
+
+	@PUT
+	@Path("/user/{userName}")
+	public void addUser() throws JargonException {
+		log.info("addUser()");
 	}
 
 }
