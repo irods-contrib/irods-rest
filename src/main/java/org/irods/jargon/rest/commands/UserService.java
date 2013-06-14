b59e5459c053c2cee1ccb2aaa9611bd13375edc5
mconway <michael.c.conway@gmail.com>
2013-06-14 13:48:18 -0400

    [#1478] initial dev and setup

--- src/main/java/org/irods/jargon/rest/commands/UserService.java
+++ src/main/java/org/irods/jargon/rest/commands/UserService.java
@@ -11,8 +11,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
+import org.irods.jargon.core.connection.IRODSAccount;
 import org.irods.jargon.core.exception.JargonException;
 import org.irods.jargon.core.pub.IRODSAccessObjectFactory;
+import org.irods.jargon.core.pub.UserAO;
 import org.irods.jargon.core.pub.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -58,7 +60,14 @@ public class UserService {
 			throw new IllegalArgumentException("null or empty userName");
 		}
 		
-		return new User();
+		IRODSAccount irodsAccount = IRODSAccount.instance("localhost", 1247, "test1", "test",
+				"", "test1", "test1-resc");
+		
+		UserAO userAO = irodsAccessObjectFactory.getUserAO(irodsAccount);
+		log.info("looking up user with name:{}", userName);
+		
+		
+		return userAO.findByName(userName);
 	}
 
 	@PUT
