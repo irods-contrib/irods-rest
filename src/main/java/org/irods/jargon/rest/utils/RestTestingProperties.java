143b9ebaf477683ed1be6af2cb59aeadd91abdc7
mconway <michael_conway@unc.edu>
2013-06-17 12:59:51 -0400

    [#1478] xml/json content negotiation

---
+++ src/main/java/org/irods/jargon/rest/utils/RestTestingProperties.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.irods.jargon.rest.utils;
+
+/**
+ * Handy properties used in testing these services
+ * @author Mike Conway - DICE (www.irods.org)
+ *
+ */
+public class RestTestingProperties {
+	public static final String REST_PORT_PROPERTY = "test.rest.port";
+
+}
