34df804d22c349bd39f4df62960739f7c23266f1
mconway <michael_conway@unc.edu>
2014-02-27 12:50:30 -0500

    fix readme to github stds

--- src/main/java/org/irods/jargon/rest/utils/RestVersion.java
+++ src/main/java/org/irods/jargon/rest/utils/RestVersion.java
@@ -1,5 +1,5 @@
 package org.irods.jargon.rest.utils;
 public final class RestVersion {
  public static String VERSION="0.0.1-SNAPSHOT";
- public static String BUILD_TIME="20140224-1353";
+ public static String BUILD_TIME="20140227-1247";
 }
