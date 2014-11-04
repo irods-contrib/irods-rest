8cdfe33e71eee13efe68e3695309147425b499d6
mconway <michael_conway@unc.edu>
2014-11-04 10:00:38 -0500

    #2 adding CORS support

--- src/main/java/org/irods/jargon/rest/configuration/IrodsCorsFilter.java
+++ src/main/java/org/irods/jargon/rest/configuration/IrodsCorsFilter.java
@@ -4,13 +4,13 @@
 package org.irods.jargon.rest.configuration;
 
 import java.io.IOException;
-import java.util.Set;
 
+import javax.inject.Named;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.ext.Provider;
 
-import org.jboss.resteasy.annotations.interception.ServerInterceptor;
 import org.jboss.resteasy.plugins.interceptors.CorsFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,30 +19,35 @@ import org.slf4j.LoggerFactory;
  * @author Mike Conway - DICE
  * 
  */
+@Named
 @Provider
-@ServerInterceptor
+@PreMatching
 public class IrodsCorsFilter extends CorsFilter {
 
 	private final Logger log = LoggerFactory.getLogger(IrodsCorsFilter.class);
 
+	/**
+	 * This is poorly documented in RESTEasy, I'm sure this is not exactly
+	 * right, but at least this is workable! Somebody smarter and more patent
+	 * than me can make this 'proper'. I decided to fly in info from
+	 * RestConfiguration, which in many ways seems tighter.
+	 */
 	@Override
 	public void filter(ContainerRequestContext requestContext,
 			ContainerResponseContext responseContext) throws IOException {
 
-		log.info("cors filter fired");
-		super.filter(requestContext, responseContext);
-	}
+		log.info("cors filter fired (request,response)");
+		// super.filter(requestContext, responseContext);
 
-	@Override
-	public void filter(ContainerRequestContext requestContext)
-			throws IOException {
-		log.info("cors filter fired");
-		super.filter(requestContext);
-	}
+		responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
 
-	@Override
-	public Set<String> getAllowedOrigins() {
-		return super.getAllowedOrigins();
+		responseContext.getHeaders().add("Access-Control-Allow-Credentials",
+				"true");
+
+		responseContext.getHeaders().add("Access-Control-Allow-Methods",
+				"GET, POST, DELETE, PUT");
+
+		log.info("set response headers:{}", responseContext.getHeaders());
 	}
 
 }
--- src/main/java/org/irods/jargon/rest/configuration/RestConfiguration.java
+++ src/main/java/org/irods/jargon/rest/configuration/RestConfiguration.java
@@ -3,6 +3,9 @@
  */
 package org.irods.jargon.rest.configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Pojo containing configuration information
  * 
@@ -19,6 +22,22 @@ public class RestConfiguration {
 	private boolean smimeEncryptAdminFunctions = false;
 	private String privateCertAbsPath = "";
 	private String publicKeyAbsPath = "";
+	/**
+	 * Add CORS headers
+	 */
+	private boolean allowCors = false;
+	/**
+	 * Allowed CORS origins
+	 */
+	private List<String> corsOrigins = new ArrayList<String>();
+	/**
+	 * Allowed CORS methods
+	 */
+	private List<String> corsMethods = new ArrayList<String>();
+	/**
+	 * Allow CORDS credentials
+	 */
+	private boolean corsAllowCredentials = false;
 
 	/**
 	 * Optional URL for a web interface to access grid data (typically an
@@ -153,6 +172,138 @@ public class RestConfiguration {
 
 	public void setWebInterfaceURL(final String webInterfaceURL) {
 		this.webInterfaceURL = webInterfaceURL;
+	}
+
+	/**
+	 * @return the allowCors
+	 */
+	public boolean isAllowCors() {
+		return allowCors;
+	}
+
+	/**
+	 * @param allowCors
+	 *            the allowCors to set
+	 */
+	public void setAllowCors(boolean allowCors) {
+		this.allowCors = allowCors;
+	}
+
+	/**
+	 * @return the corsOrigins
+	 */
+	public List<String> getCorsOrigins() {
+		return corsOrigins;
+	}
+
+	/**
+	 * @param corsOrigins
+	 *            the corsOrigins to set
+	 */
+	public void setCorsOrigins(List<String> corsOrigins) {
+		this.corsOrigins = corsOrigins;
+	}
+
+	/**
+	 * @return the corsMethods
+	 */
+	public List<String> getCorsMethods() {
+		return corsMethods;
+	}
+
+	/**
+	 * @param corsMethods
+	 *            the corsMethods to set
+	 */
+	public void setCorsMethods(List<String> corsMethods) {
+		this.corsMethods = corsMethods;
+	}
+
+	/**
+	 * @return the corsAllowCredentials
+	 */
+	public boolean isCorsAllowCredentials() {
+		return corsAllowCredentials;
+	}
+
+	/**
+	 * @param corsAllowCredentials
+	 *            the corsAllowCredentials to set
+	 */
+	public void setCorsAllowCredentials(boolean corsAllowCredentials) {
+		this.corsAllowCredentials = corsAllowCredentials;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		final int maxLen = 10;
+		StringBuilder builder = new StringBuilder();
+		builder.append("RestConfiguration [");
+		if (irodsHost != null) {
+			builder.append("irodsHost=");
+			builder.append(irodsHost);
+			builder.append(", ");
+		}
+		builder.append("irodsPort=");
+		builder.append(irodsPort);
+		builder.append(", ");
+		if (irodsZone != null) {
+			builder.append("irodsZone=");
+			builder.append(irodsZone);
+			builder.append(", ");
+		}
+		if (defaultStorageResource != null) {
+			builder.append("defaultStorageResource=");
+			builder.append(defaultStorageResource);
+			builder.append(", ");
+		}
+		if (realm != null) {
+			builder.append("realm=");
+			builder.append(realm);
+			builder.append(", ");
+		}
+		builder.append("smimeEncryptAdminFunctions=");
+		builder.append(smimeEncryptAdminFunctions);
+		builder.append(", ");
+		if (privateCertAbsPath != null) {
+			builder.append("privateCertAbsPath=");
+			builder.append(privateCertAbsPath);
+			builder.append(", ");
+		}
+		if (publicKeyAbsPath != null) {
+			builder.append("publicKeyAbsPath=");
+			builder.append(publicKeyAbsPath);
+			builder.append(", ");
+		}
+		builder.append("allowCors=");
+		builder.append(allowCors);
+		builder.append(", ");
+		if (corsOrigins != null) {
+			builder.append("corsOrigins=");
+			builder.append(corsOrigins.subList(0,
+					Math.min(corsOrigins.size(), maxLen)));
+			builder.append(", ");
+		}
+		if (corsMethods != null) {
+			builder.append("corsMethods=");
+			builder.append(corsMethods.subList(0,
+					Math.min(corsMethods.size(), maxLen)));
+			builder.append(", ");
+		}
+		builder.append("corsAllowCredentials=");
+		builder.append(corsAllowCredentials);
+		builder.append(", ");
+		if (webInterfaceURL != null) {
+			builder.append("webInterfaceURL=");
+			builder.append(webInterfaceURL);
+		}
+		builder.append("]");
+		return builder.toString();
 	}
 
 }
