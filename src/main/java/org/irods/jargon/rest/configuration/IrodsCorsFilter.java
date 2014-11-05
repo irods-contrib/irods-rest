c8313700e2fbf9b765568aa21e611b84619cc01c
mconway <michael.c.conway@gmail.com>
2014-11-05 09:55:06 -0500

    #2 update cors filter

--- src/main/java/org/irods/jargon/rest/configuration/IrodsCorsFilter.java
+++ src/main/java/org/irods/jargon/rest/configuration/IrodsCorsFilter.java
@@ -1,93 +1,262 @@
-/**
- * 
- */
 package org.irods.jargon.rest.configuration;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.inject.Inject;
-import javax.inject.Named;
+import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.container.PreMatching;
-import javax.ws.rs.ext.Provider;
+import javax.ws.rs.core.Response;
 
-import org.jboss.resteasy.plugins.interceptors.CorsFilter;
+import org.jboss.resteasy.spi.CorsHeaders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * @author Mike Conway - DICE
- * 
+ * Handles CORS requests both preflight and simple CORS requests.
  */
-@Named
-@Provider
 @PreMatching
-public class IrodsCorsFilter extends CorsFilter {
+public class IrodsCorsFilter implements ContainerRequestFilter,
+		ContainerResponseFilter {
 
 	private final Logger log = LoggerFactory.getLogger(IrodsCorsFilter.class);
 
 	@Inject
 	private RestConfiguration restConfiguration;
 
+	private boolean allowCredentials = true;
+	private String allowedMethods;
+	private String allowedHeaders;
+	private String exposedHeaders;
+	private int corsMaxAge = -1;
+	private Set<String> allowedOrigins = new HashSet<String>();
+
 	/**
-	 * This is poorly documented in RESTEasy, I'm sure this is not exactly
-	 * right, but at least this is workable! Somebody smarter and more patent
-	 * than me can make this 'proper'. I decided to fly in info from
-	 * RestConfiguration, which in many ways seems tighter.
+	 * Put "*" if you want to accept all origins
+	 *
+	 * @return
 	 */
+	public synchronized Set<String> getAllowedOrigins() {
+		return allowedOrigins;
+	}
+
+	/**
+	 * Defaults to true
+	 *
+	 * @return
+	 */
+	public synchronized boolean isAllowCredentials() {
+		return allowCredentials;
+	}
+
+	public synchronized void setAllowCredentials(boolean allowCredentials) {
+		this.allowCredentials = allowCredentials;
+	}
+
+	/**
+	 * Will allow all by default
+	 *
+	 * @return
+	 */
+	public synchronized String getAllowedMethods() {
+		return allowedMethods;
+	}
+
+	/**
+	 * Will allow all by default comma delimited string for
+	 * Access-Control-Allow-Methods
+	 *
+	 * @param allowedMethods
+	 */
+	public synchronized void setAllowedMethods(String allowedMethods) {
+		this.allowedMethods = allowedMethods;
+	}
+
+	public synchronized String getAllowedHeaders() {
+		return allowedHeaders;
+	}
+
+	/**
+	 * Will allow all by default comma delimited string for
+	 * Access-Control-Allow-Headers
+	 *
+	 * @param allowedHeaders
+	 */
+	public synchronized void setAllowedHeaders(String allowedHeaders) {
+		this.allowedHeaders = allowedHeaders;
+	}
+
+	public synchronized int getCorsMaxAge() {
+		return corsMaxAge;
+	}
+
+	public synchronized void setCorsMaxAge(int corsMaxAge) {
+		this.corsMaxAge = corsMaxAge;
+	}
+
+	public synchronized String getExposedHeaders() {
+		return exposedHeaders;
+	}
+
+	/**
+	 * comma delimited list
+	 *
+	 * @param exposedHeaders
+	 */
+	public synchronized void setExposedHeaders(String exposedHeaders) {
+		this.exposedHeaders = exposedHeaders;
+	}
+
+	@Override
+	public void filter(ContainerRequestContext requestContext)
+			throws IOException {
+
+		log.debug("filter(requestContext)");
+
+		if (!isAllowCors()) {
+			log.debug("no CORS");
+			return;
+		}
+
+		String origin = requestContext.getHeaderString(CorsHeaders.ORIGIN);
+		if (origin == null) {
+			log.debug("no origin in header, ignore");
+			return;
+		}
+		if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
+			preflight(origin, requestContext);
+		} else {
+			checkOrigin(requestContext, origin);
+		}
+	}
+
 	@Override
 	public void filter(ContainerRequestContext requestContext,
 			ContainerResponseContext responseContext) throws IOException {
 
-		log.info("cors filter fired (request,response)");
-		// super.filter(requestContext, responseContext);
+		log.debug("filter(reqeustContext,responseContext");
 
-		if (restConfiguration == null) {
-			throw new IllegalStateException("no RestConfiguration set");
-		}
-
-		log.debug("restConfiguration:{}", restConfiguration);
-
-		if (!restConfiguration.isAllowCors()) {
-			log.debug("no CORS processing");
+		if (!isAllowCors()) {
+			log.debug("no CORS");
 			return;
 		}
 
+		String origin = requestContext.getHeaderString(CorsHeaders.ORIGIN);
+		if (origin == null
+				|| requestContext.getMethod().equalsIgnoreCase("OPTIONS")
+				|| requestContext.getProperty("cors.failure") != null) {
+			// don't do anything if origin is null, its an OPTIONS request, or
+			// cors.failure is set
+			log.debug("no origin, not options, or cords.failure is set..ignore");
+
+			return;
+		}
+		responseContext.getHeaders().putSingle(
+				CorsHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, origin);
+		if (allowCredentials)
+			responseContext.getHeaders().putSingle(
+					CorsHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+
+		if (exposedHeaders != null) {
+			responseContext.getHeaders().putSingle(
+					CorsHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, exposedHeaders);
+		}
+	}
+
+	protected void preflight(String origin,
+			ContainerRequestContext requestContext) throws IOException {
+
+		log.debug("preflight");
+
+		if (!isAllowCors()) {
+			log.debug("no CORS");
+			return;
+		}
+
+		log.info("check origin");
+		checkOrigin(requestContext, origin);
+
+		Response.ResponseBuilder builder = Response.ok();
+		builder.header(CorsHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, origin);
+		if (allowCredentials)
+			builder.header(CorsHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+		String requestMethods = requestContext
+				.getHeaderString(CorsHeaders.ACCESS_CONTROL_REQUEST_METHOD);
+		if (requestMethods != null) {
+			if (allowedMethods != null) {
+				requestMethods = this.allowedMethods;
+			}
+			builder.header(CorsHeaders.ACCESS_CONTROL_ALLOW_METHODS,
+					requestMethods);
+		}
+		String allowHeaders = requestContext
+				.getHeaderString(CorsHeaders.ACCESS_CONTROL_REQUEST_HEADERS);
+		if (allowHeaders != null) {
+			if (allowedHeaders != null) {
+				allowHeaders = this.allowedHeaders;
+			}
+			builder.header(CorsHeaders.ACCESS_CONTROL_ALLOW_HEADERS,
+					allowHeaders);
+		}
+		if (corsMaxAge > -1) {
+			builder.header(CorsHeaders.ACCESS_CONTROL_MAX_AGE, corsMaxAge);
+		}
+		requestContext.abortWith(builder.build());
+
+	}
+
+	protected void checkOrigin(ContainerRequestContext requestContext,
+			String origin) {
+		if (!allowedOrigins.contains("*") && !allowedOrigins.contains(origin)) {
+			requestContext.setProperty("cors.failure", true);
+			throw new ForbiddenException("Origin not allowed: " + origin);
+		}
+	}
+
+	public synchronized RestConfiguration getRestConfiguration() {
+		return restConfiguration;
+	}
+
+	public synchronized void setRestConfiguration(
+			RestConfiguration restConfiguration) {
+
+		log.info("rest configuration set:{}", restConfiguration);
+
+		this.restConfiguration = restConfiguration;
+
+		log.info(
+				"setting up cors filter parameters from new rest configuration:{}",
+				restConfiguration);
+		parseAllowedMethodsFromConfiguration();
+		parseAllowedOriginsFromConfiguration();
+		parseAllowCredentialsFromConfiguration();
+
+	}
+
+	private synchronized boolean isAllowCors() {
 		log.debug("restConfiguration:{}", restConfiguration);
 
-		if (restConfiguration.getCorsOrigins().isEmpty()) {
-			log.debug("default to all origins");
-			responseContext.getHeaders()
-					.add("Access-Control-Allow-Origin", "*");
+		if (!restConfiguration.isAllowCors()) {
+			return false;
 		} else {
-			log.debug("building up origin list");
-			StringBuilder sb = new StringBuilder();
-			int ctr = 0;
-			for (String origin : restConfiguration.getCorsOrigins()) {
-				if (ctr++ > 0) {
-					sb.append(',');
-				}
-				sb.append(origin);
-			}
-			responseContext.getHeaders().add("Access-Control-Allow-Origin",
-					sb.toString());
-
+			return true;
 		}
 
-		if (restConfiguration.isCorsAllowCredentials()) {
-			log.debug("allow credentials");
-			responseContext.getHeaders().add(
-					"Access-Control-Allow-Credentials", "true");
-		}
+	}
 
+	private void parseAllowedMethodsFromConfiguration() {
+		StringBuilder sb = new StringBuilder();
 		if (restConfiguration.getCorsMethods().isEmpty()) {
 			log.info("no methods specified, add all");
-			responseContext.getHeaders().add("Access-Control-Allow-Methods",
-					"GET, POST, DELETE, PUT");
+			sb.append("GET, POST, DELETE, PUT");
 		} else {
 			log.debug("building up cords methods list");
-			StringBuilder sb = new StringBuilder();
+
 			int ctr = 0;
 			for (String method : restConfiguration.getCorsMethods()) {
 				if (ctr++ > 0) {
@@ -95,27 +264,31 @@ public class IrodsCorsFilter extends CorsFilter {
 				}
 				sb.append(method);
 			}
-			responseContext.getHeaders().add("Access-Control-Allow-Methods",
-					sb.toString());
 
 		}
 
-		log.info("set response headers:{}", responseContext.getHeaders());
+		this.setAllowedMethods(sb.toString());
 	}
 
-	/**
-	 * @return the restConfiguration
-	 */
-	public RestConfiguration getRestConfiguration() {
-		return restConfiguration;
+	public void parseAllowedOriginsFromConfiguration() {
+
+		Set<String> allowedOrigins = new HashSet<String>();
+		if (restConfiguration.getCorsOrigins().isEmpty()) {
+			log.debug("default to all origins");
+			allowedOrigins.add("*");
+		} else {
+			log.debug("building up origin list");
+			for (String origin : restConfiguration.getCorsOrigins()) {
+				allowedOrigins.add(origin);
+			}
+		}
+
+		this.allowedOrigins = allowedOrigins;
+
 	}
 
-	/**
-	 * @param restConfiguration
-	 *            the restConfiguration to set
-	 */
-	public void setRestConfiguration(RestConfiguration restConfiguration) {
-		this.restConfiguration = restConfiguration;
+	public void parseAllowCredentialsFromConfiguration() {
+		this.allowCredentials = restConfiguration.isCorsAllowCredentials();
 	}
 
 }
--- src/test/java/org/irods/jargon/rest/configuration/IrodsCorsFilterTest.java
+++ src/test/java/org/irods/jargon/rest/configuration/IrodsCorsFilterTest.java
@@ -3,6 +3,7 @@ package org.irods.jargon.rest.configuration;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.core.MultivaluedHashMap;
@@ -10,6 +11,7 @@ import javax.ws.rs.core.MultivaluedMap;
 
 import junit.framework.Assert;
 
+import org.jboss.resteasy.spi.CorsHeaders;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -28,17 +30,73 @@ public class IrodsCorsFilterTest {
 		MultivaluedMap<String, Object> map = new MultivaluedHashMap<String, Object>();
 		Mockito.when(responseContext.getHeaders()).thenReturn(map);
 
+		Mockito.when(requestContext.getHeaderString(CorsHeaders.ORIGIN))
+				.thenReturn("blah");
+
+		Mockito.when(requestContext.getMethod()).thenReturn("PUT");
+
 		IrodsCorsFilter filter = new IrodsCorsFilter();
 		filter.setRestConfiguration(config);
 
 		filter.filter(requestContext, responseContext);
 		Assert.assertFalse("no headers set", responseContext.getHeaders()
 				.isEmpty());
-		Assert.assertEquals("did not set default cors origin", "[*]",
-				map.get("Access-Control-Allow-Origin").toString());
-		Assert.assertEquals("did not set default cors origin",
-				"[GET, POST, DELETE, PUT]",
-				map.get("Access-Control-Allow-Methods").toString());
+		Assert.assertEquals("did not set default cors origin", "[blah]", map
+				.get("Access-Control-Allow-Origin").toString());
+
+	}
+
+	@Test(expected = ForbiddenException.class)
+	public void testFilterWithCorsOriginListBadOriginOPreFlight()
+			throws Exception {
+		RestConfiguration config = new RestConfiguration();
+
+		List<String> origins = new ArrayList<String>();
+		origins.add("bob");
+		origins.add("sally");
+
+		config.setAllowCors(true);
+		config.setCorsAllowCredentials(true);
+		config.setCorsOrigins(origins);
+
+		ContainerRequestContext requestContext = Mockito
+				.mock(ContainerRequestContext.class);
+
+		Mockito.when(requestContext.getHeaderString(CorsHeaders.ORIGIN))
+				.thenReturn("steve");
+
+		Mockito.when(requestContext.getMethod()).thenReturn("OPTIONS");
+		IrodsCorsFilter filter = new IrodsCorsFilter();
+		filter.setRestConfiguration(config);
+
+		filter.filter(requestContext);
+
+	}
+
+	@Test
+	public void testFilterWithCorsOriginListBadOriginOPreFlightCorsOff()
+			throws Exception {
+		RestConfiguration config = new RestConfiguration();
+
+		List<String> origins = new ArrayList<String>();
+		origins.add("bob");
+		origins.add("sally");
+
+		config.setAllowCors(false);
+		config.setCorsAllowCredentials(true);
+		config.setCorsOrigins(origins);
+
+		ContainerRequestContext requestContext = Mockito
+				.mock(ContainerRequestContext.class);
+
+		Mockito.when(requestContext.getHeaderString(CorsHeaders.ORIGIN))
+				.thenReturn("steve");
+
+		Mockito.when(requestContext.getMethod()).thenReturn("OPTIONS");
+		IrodsCorsFilter filter = new IrodsCorsFilter();
+		filter.setRestConfiguration(config);
+
+		filter.filter(requestContext);
 
 	}
 
@@ -61,17 +119,23 @@ public class IrodsCorsFilterTest {
 		MultivaluedMap<String, Object> map = new MultivaluedHashMap<String, Object>();
 		Mockito.when(responseContext.getHeaders()).thenReturn(map);
 
+		Mockito.when(requestContext.getHeaderString(CorsHeaders.ORIGIN))
+				.thenReturn("bob");
+
+		Mockito.when(requestContext.getMethod()).thenReturn("PUT");
 		IrodsCorsFilter filter = new IrodsCorsFilter();
 		filter.setRestConfiguration(config);
 
 		filter.filter(requestContext, responseContext);
 		Assert.assertFalse("no headers set", responseContext.getHeaders()
 				.isEmpty());
-		Assert.assertEquals("did not set default cors origin", "[bob,sally]",
-				map.get("Access-Control-Allow-Origin").toString());
-		Assert.assertEquals("did not set default cors origin",
-				"[GET, POST, DELETE, PUT]",
-				map.get("Access-Control-Allow-Methods").toString());
+		Assert.assertEquals("did not set default cors origin", "[bob]", map
+				.get("Access-Control-Allow-Origin").toString());
+		/*
+		 * Assert.assertEquals("did not set default cors origin",
+		 * "[GET, POST, DELETE, PUT]",
+		 * map.get("Access-Control-Allow-Methods").toString());
+		 */
 
 	}
 
@@ -93,36 +157,6 @@ public class IrodsCorsFilterTest {
 
 		filter.filter(requestContext, responseContext);
 		Assert.assertTrue("headers set", responseContext.getHeaders().isEmpty());
-
-	}
-
-	@Test
-	public void testFilterWithCorsActionList() throws Exception {
-		RestConfiguration config = new RestConfiguration();
-
-		List<String> actions = new ArrayList<String>();
-		actions.add("POST");
-		actions.add("PUT");
-
-		config.setAllowCors(true);
-		config.setCorsAllowCredentials(true);
-		config.setCorsMethods(actions);
-
-		ContainerRequestContext requestContext = Mockito
-				.mock(ContainerRequestContext.class);
-		ContainerResponseContext responseContext = Mockito
-				.mock(ContainerResponseContext.class);
-		MultivaluedMap<String, Object> map = new MultivaluedHashMap<String, Object>();
-		Mockito.when(responseContext.getHeaders()).thenReturn(map);
-
-		IrodsCorsFilter filter = new IrodsCorsFilter();
-		filter.setRestConfiguration(config);
-
-		filter.filter(requestContext, responseContext);
-		Assert.assertFalse("no headers set", responseContext.getHeaders()
-				.isEmpty());
-		Assert.assertEquals("did not set methods", "[POST,PUT]",
-				map.get("Access-Control-Allow-Methods").toString());
 
 	}
 
