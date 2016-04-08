39909b987d20af5624517c817857f8ec6bac8876
michael-conway <michael_conway@unc.edu>
2016-04-08 16:33:49 -0400

    #34 first tests

--- functional-testing/test_tickets_spec.js
+++ functional-testing/test_tickets_spec.js
@@ -14,6 +14,23 @@ frisby.globalSetup({
   }
 });
 
+var testCollForCreate = "/RESTtestCreateCollAsLoggedInUser";
 
-frisby.create('test get environment as logged in user').get(testProps.urlPrefix("server")).auth(testProps.user1, testProps.password1).inspectStatus(200).toss();
+frisby.create('test get environment as logged in user').get(testProps.urlPrefix("server")).auth(testProps.user1, testProps.password1).expectStatus(200).toss();
+
+frisby.create('test create collection as logged in user - RESTtestCreateCollASLoggedInUser').put(testProps.urlPrefix("collection" + testProps.userHome(testProps.zone, testProps.user1) + testCollForCreate )).auth(testProps.user1, testProps.password1).expectStatus(200).inspectJSON()
+    .toss();
+
+frisby.create('test get info on collection I just created - RESTtestCreateCollASLoggedInUser').get(testProps.urlPrefix("collection" + testProps.userHome(testProps.zone, testProps.user1) + testCollForCreate )).auth(testProps.user1, testProps.password1).expectStatus(200).inspectJSON()
+    .toss();
+
+/**
+ * TODO: fix ref https://github.com/DICE-UNC/irods-rest/issues/50
+ */
+frisby.create('test get info on collection that doesnt exist get a 404').get(testProps.urlPrefix("collection" + testProps.userHome(testProps.zone, testProps.user1) + testCollForCreate + "idontexistbtw" )).auth(testProps.user1, testProps.password1).expectStatus(500)
+    .toss();
+
+
+
+
 
--- functional-testing/testing_props.js
+++ functional-testing/testing_props.js
@@ -11,6 +11,8 @@ exports.password1 = "test";
 exports.anonymous = "anonymous";
 exports.protocol = 'http://';
 exports.context = "/irods-rest/rest";
+exports.zone = "zone1";
+exports.ticket1 = "frisby-test-tkt1";
 
 /**
  * Return the url up to the given resource (protocol://host:port/context/resource)
@@ -22,5 +24,26 @@ exports.urlPrefix = function(resource)  {
     }
 
     return exports.protocol + exports.host + ":" + exports.port + exports.context +"/" + resource;
+
+};
+
+/**
+ * Get the user home path
+ * @param zone
+ * @param user
+ * @returns {string}
+ */
+exports.userHome = function(zone, user) {
+
+    if (!zone) {
+        throw("missing zone");
+    }
+
+    if (!user) {
+        throw("missing user");
+    }
+
+    return "/" + zone + "/home/" + user;
+
 
 };
