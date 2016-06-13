7f0b71b7cc50db9506c059b925f6f179e4049f47
michael-conway <michael_conway@unc.edu>
2016-06-13 16:29:17 -0400

    #34 testing of tickets

--- functional-testing/test_tickets_spec.js
+++ functional-testing/test_tickets_spec.js
@@ -22,6 +22,8 @@ var testCollForCreate = "/RESTtestCreateCollAsLoggedInUser";
 var ticketFolder =  testProps.userHome(testProps.zone, testProps.user1) + testCollForCreate;
 var ticketString1 = "RESTtest-ticket1";
 
+/*
+
 frisby.create('test get environment as logged in user').get(testProps.urlPrefix("server")).auth(testProps.user1, testProps.password1).expectStatus(200).toss();
 
 frisby.create('test create collection as logged in user - RESTtestCreateCollASLoggedInUser').put(testProps.urlPrefix("collection" + testProps.userHome(testProps.zone, testProps.user1) + testCollForCreate )).auth(testProps.user1, testProps.password1).expectStatus(200).inspectJSON()
@@ -30,21 +32,26 @@ frisby.create('test create collection as logged in user - RESTtestCreateCollASLo
 frisby.create('test get info on collection I just created - RESTtestCreateCollASLoggedInUser').get(testProps.urlPrefix("collection" + testProps.userHome(testProps.zone, testProps.user1) + testCollForCreate )).auth(testProps.user1, testProps.password1).expectStatus(200).inspectJSON()
     .toss();
 
+    */
+
 /**
  * TODO: fix ref https://github.com/DICE-UNC/irods-rest/issues/50
  */
+
+/*
 frisby.create('test get info on collection that doesnt exist get a 404').get(testProps.urlPrefix("collection" + testProps.userHome(testProps.zone, testProps.user1) + testCollForCreate + "idontexistbtw" )).auth(testProps.user1, testProps.password1).expectStatus(500)
     .toss();
 
-
+*/
 frisby.create("delete a ticket on your collection").delete(testProps.urlPrefix("ticket/" + ticketString1)).auth(testProps.user1, testProps.password1).expectStatus(204)
     .toss();
 
-frisby.create("add a ticket on your collection").post(testProps.urlPrefix("ticket"), {createTicketRequestData:{
+
+frisby.create("add a ticket on your collection").post(testProps.urlPrefix("ticket"), {
         mode: 'read',
         object_path: ticketFolder,
         ticket_string: ticketString1}
-    }, {json: true},{ headers: { "Content-Type": "application/json"}}).auth(testProps.user1, testProps.password1).expectStatus(200).inspectRequest().inspectBody()
+    , {json: true},{ headers: { "Content-Type": "application/json"}}).auth(testProps.user1, testProps.password1).expectStatus(200).inspectRequest().inspectBody()
     .expectHeaderContains('Content-Type', 'application/json').inspectJSON()
     .toss();
 
@@ -55,7 +62,7 @@ form.append('uploadFile',fs.createReadStream(testProps.testfile1Path()), {
 });
 
 var uploadFileName = "upload1.txt";
-/*
+
 frisby.create('file upload with ticket, anonymous')
     .post(testProps.urlPrefix("fileContents" + ticketFolder + "/" + uploadFileName + "?ticket=" + ticketString1), form, {
         json: false,
@@ -66,4 +73,4 @@ frisby.create('file upload with ticket, anonymous')
     })
     .expectStatus(200)
     .inspectJSON()
-    .toss();*/
+    .toss();
